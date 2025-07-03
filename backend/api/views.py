9c5351bf51425320733c93dfe7041c1cc98fb2cd
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-07-03 13:44:35 +0530

    Echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
 
-# Create your views here.
+
+
+def api_home(request, *args, **kwargs):
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    print(request.GET)
+    print(request.POST)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    # data['headers'] = request.headers
+    # print(request.headers)
+    data['headers'] = dict(request.headers)
+    # json.dumps(dict(request.headers))
+    data['content_type'] = request.content_type
+
+    # return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    return JsonResponse(data)
